00f03e09c576c4d2b0b1820f8ac50cc2fb3c6c6d
weatherbetter12@gmail.com <weatherbetter12@gmail.com>
2023-07-12 11:27:24 +0900

    style: check format

--- example/views.py
+++ example/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpRequest, JsonResponse
+from django.shortcuts import render
 
-def index(request:HttpRequest):
-    response = {'message' : "Hello world"}
+
+def index(request: HttpRequest):
+    response = {"message": "Hello world"}
     return JsonResponse(response)
