505e7b71f1b05a250772bf643d47707a2da5922c
sinabiber <chocodufu@gmail.com>
2023-08-01 10:23:11 +0900

    feature: 인명구조장비함 urls.py

--- example/urls.py
+++ example/urls.py
@@ -1,6 +1,14 @@
-from . import views
+# from . import views
+# from django.urls import path
+
+# urlpatterns = [
+#     path("", views.index, name="example_index")
+# ]
+
 from django.urls import path
+from .views import map_markers
 
 urlpatterns = [
-    path("", views.index, name="example_index")
+    path('api/markers/', map_markers, name='map_markers'),
+    # 다른 URL 패턴들을 추가할 수 있습니다.
 ]
