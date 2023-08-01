296e8bf14dae20f9caa5d95d6ccc7b415d309ad4
sinabiber <chocodufu@gmail.com>
2023-08-01 17:58:30 +0900

    Revert "feature: 인명구조장비함 urls.py"
    This reverts commit 505e7b71f1b05a250772bf643d47707a2da5922c.

--- example/urls.py
+++ example/urls.py
@@ -1,14 +1,6 @@
-# from . import views
-# from django.urls import path
-
-# urlpatterns = [
-#     path("", views.index, name="example_index")
-# ]
-
+from . import views
 from django.urls import path
-from .views import map_markers
 
 urlpatterns = [
-    path('api/markers/', map_markers, name='map_markers'),
-    # 다른 URL 패턴들을 추가할 수 있습니다.
+    path("", views.index, name="example_index")
 ]
