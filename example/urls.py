7df5457b6488f0a69351927587b2800b788deb15
yeringii <yeringii@kookmin.ac.kr>
2023-08-03 13:11:04 +0900

    feature: add accident API

--- example/urls.py
+++ example/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.index, name="example_index"),
-    path('admin/', admin.site.urls)
+    # path('admin/', admin.site.urls),
+    path('accident/', views.accidentApi, name="example_accident"),
 ]
