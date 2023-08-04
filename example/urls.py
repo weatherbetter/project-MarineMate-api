1310547accbce15acede727d5b3775ae6ec9d24e
yeringii <yeringii@kookmin.ac.kr>
2023-08-04 22:04:25 +0900

    feature: add 응급인프라api

--- example/urls.py
+++ example/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.urls import path
 
 from . import views
-from .views import equipmentAPI
 
 urlpatterns = [
     path("", views.index, name="example_index"),
     # path('admin/', admin.site.urls),
     path("accident/", views.accidentApi, name="example_accident"),
-    path("equipment/", equipmentAPI),
+    path("equipment/", views.equipmentApi, name="example_equipment"),
+    path("safety/", views.safetyApi, name="example_safety"),
 ]
--- example/views.py
+++ example/views.py
@@ -1,24 +1,16 @@
-# from .serializers import AccidentSerializer,AccidentMonthSerializer
-from django.db.models import Count
-from django.http import HttpRequest, JsonResponse
-from django.shortcuts import render
-from rest_framework import status
-from rest_framework.decorators import api_view
-
-# drf
 from rest_framework.request import Request
 from rest_framework.response import Response
-
-from .models import Accident, LifesavingEquipment
+from .models import Accident, LifesavingEquipment, SafetyInfra
 from .serializers import EquipmentSerializer
+from django.db.models import Count
+from django.http import HttpRequest, JsonResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 
 def index(request: HttpRequest):
     response = {"message": "Hi world it is updated!?"}
     return JsonResponse(response)
-
-
-# 수난사고 현황 API
 
 
 @api_view(http_method_names=["GET"])
@@ -81,7 +73,28 @@ def accidentApi(request: Request):
 
 
 @api_view(["GET"])
-def equipmentAPI(request):
+def equipmentApi(request):
     totalequipment = LifesavingEquipment.objects.all()  # 모델로 만들어진 객체를 모두 가져오기
     serializer = EquipmentSerializer(totalequipment, many=True)  # 다양한 내용들에 대해 내부적으로도 직렬화
     return Response(serializer.data)
+
+
+@api_view(http_method_names=["GET"])
+def safetyApi(request: Request):
+    if "location" in request.GET:
+        
+        # 소방서 수
+        fs_cnt = SafetyInfra.objects.filter(location=request.GET["location"]).values("fs_cnt")
+        # 안전센터 수
+        sc_cnt = SafetyInfra.objects.filter(location=request.GET["location"]).values("sc_cnt")
+        # 펌뷸런스 수
+        fb_cnt = SafetyInfra.objects.filter(location=request.GET["location"]).values("fb_cnt")
+  
+        response = {
+            "Fire Station" : fs_cnt[0]['fs_cnt'],
+            "Safety Center" : sc_cnt[0]['sc_cnt'],
+            "Pumbulance" : fb_cnt[0]['fb_cnt'],
+        }
+        
+    return Response(data=response, status=status.HTTP_200_OK)
+
